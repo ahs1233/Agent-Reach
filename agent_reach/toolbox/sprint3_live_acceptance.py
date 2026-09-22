d17bf63d9268a8111d53bb64394703c1b7000937
ahs1233 <ah.hero95@gmail.com>
2026-09-22 14:07:26 +0300

    style(research): format Sprint 3 live acceptance imports

--- agent_reach/toolbox/sprint3_live_acceptance.py
+++ agent_reach/toolbox/sprint3_live_acceptance.py
@@ -12,7 +12,9 @@ import sys
 from typing import Any
 
 from agent_reach.toolbox.gateway import AhmedToolboxGateway
-from agent_reach.toolbox.sprint2_live_acceptance import run_acceptance as run_sprint2
+from agent_reach.toolbox.sprint2_live_acceptance import (
+    run_acceptance as run_sprint2,
+)
 
 
 AS_OF = "2026-09-22T12:00:00Z"
